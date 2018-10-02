9308d5ebd93f05a855ba97f9651b86e60a99eea1
Brandon McBride <brandon.mcbride4@gmail.com>
2018-10-02 09:25:11 -0600

    Added a ros_local function

--- eowyn.bash
+++ eowyn.bash
@@ -5,3 +5,11 @@ function ros_eowyn() {
     env | grep ROS_MASTER_URI
     env | grep ROS_IP
 }
+
+function ros_local() {
+    export ROS_MASTER_URI=http://localhost:11311
+    export ROS_IP=$(hostname -I)
+
+    env | grep ROS_MASTER_URI
+    env | grep ROS_IP
+}
