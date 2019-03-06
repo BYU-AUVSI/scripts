7a7b499b2a1278224e60c6c4fc4149ce411b9121
Austin Hurst <austinhurst@msn.com>
2019-03-06 16:55:13 +0000

    Added scripts for the payload drop

--- eowyn.bash
+++ eowyn.bash
@@ -13,3 +13,16 @@ function ros_local() {
     env | grep ROS_MASTER_URI
     env | grep ROS_IP
 }
+
+function drop_bomb() {
+    rosservice call /arm_bomb
+    sleep 0.5
+    rosservice call /actuate_drop_bomb
+}
+
+function gpio_drop(){
+    gpio mode 0 out
+    gpio write 0 0
+    sleep 0.12
+    gpio write 0 1
+}
