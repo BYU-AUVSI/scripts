04b2f93ad8ac0e84a0a96fa5191aa23ea4c22048
BYU-AUVSI <byu-auvsi@tryemailingme.com>
2019-06-13 13:03:44 +0000

    add ROS_HOSTNAME setting to eowyn.bash, like in the master branch

--- eowyn.bash
+++ eowyn.bash
@@ -1,17 +1,21 @@
 function ros_eowyn() {
     export ROS_MASTER_URI=http://192.168.1.8:11311
     export ROS_IP=$(hostname -I)
+    export ROS_HOSTNAME=$(hostname -I)
 
     env | grep ROS_MASTER_URI
     env | grep ROS_IP
+    env | grep ROS_HOSTNAME
 }
 
 function ros_local() {
     export ROS_MASTER_URI=http://localhost:11311
     export ROS_IP=$(hostname -I)
+    export ROS_HOSTNAME=$(hostname -I)
 
     env | grep ROS_MASTER_URI
     env | grep ROS_IP
+    env | grep ROS_HOSTNAME
 }
 
 alias send_to_plane="git push && hub pull-request -b plane -m"
